35a953b2c835acd4b3ab04133a06474f7a30e6a1
bmartinez2018 <biancamartinez295@icloud.com>
2020-11-07 14:23:52 -0600

    application start

---
+++ src/components/main/index.js
@@ -0,0 +1,14 @@
+  
+import React from "react";
+import DataArea from "../DataArea/index.js";
+import "./Main.css";
+
+function Main (){
+    return (
+        <>
+        <DataArea />
+        </>
+    );
+};
+
+export default Main;
